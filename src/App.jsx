9e7fa373716892de69d74ea37f2bbdd4a20088a6
CLBaxter-W <168162275+CLBaxter-W@users.noreply.github.com>
2024-06-12 12:13:23 -0600

    worked thru lesson, added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
+import "bootstrap/dist/css/bootstrap.css";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <>
-      <div></div>
+      <div className="containerSpiff">
+        <h2>Puppy Pal List </h2>
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+        {featPupId && (
+          <div>
+            <h3>{featuredPup.name}</h3>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </>
   );
 }
